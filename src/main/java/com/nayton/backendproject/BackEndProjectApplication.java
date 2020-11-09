4c9c151c3582da895e06c638103e6634d8c2d9f9
Nayton Wilkerson <naytonwilkerson78@gmail.com>
2020-11-09 19:52:26 -0300

    Criando operacao de instanciacao do Banco de Dados

--- src/main/java/com/nayton/backendproject/BackEndProjectApplication.java
+++ src/main/java/com/nayton/backendproject/BackEndProjectApplication.java
@@ -1,13 +1,31 @@
 package com.nayton.backendproject;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BackEndProjectApplication {
+import com.nayton.backendproject.DAO.CategoriaDAO;
+import com.nayton.backendproject.domain.Categoria;
 
+@SpringBootApplication
+public class BackEndProjectApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaDAO categoriaDAO;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndProjectApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaDAO.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
